b02f4481bbe93a2d43adcdcc81940d7ad32fbca1
James Diffley <diffs1991@googlemail.com>
2024-06-22 17:19:45 +0100

    Floor movement coded

--- main.js
+++ main.js
@@ -13,21 +13,36 @@ let world = {
     height: 480,
     width: 640,
     gravity: 10,
+    speed: 2,
+    distanceTravelled: 0,
     floorTiles: [
         new Floor(0, 140)
     ],
 
-// function to continuously draw tiles as the game runs using a for loop
-draw: function() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, this.width, this.height);
-    for (let tile of this.floorTiles) {
-        let y = this.height - tile.height;
-        ctx.fillStyle = "blue";
-        ctx.fillRect(tile.x, y, tile.width, tile.height);
+    // move floor
+    moveFloor: function() {
+        for (let tile of this.floorTiles) {
+            tile.x -= this.speed;
+            this.distanceTravelled += this.speed;
+        }
+    },
+
+    // function to continuously draw tiles as the game runs using a for loop
+    draw: function() {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, this.width, this.height);
+        for (let tile of this.floorTiles) {
+            let y = this.height - tile.height;
+            ctx.fillStyle = "purple";
+            ctx.fillRect(tile.x, y, tile.width, tile.height);
+        }
     }
-}
 };
 
-// Call the draw function
-world.draw();
+// Call the draw function and set time
+function tick() {
+    world.draw();
+    world.moveFloor();
+    window.setTimeout(tick, 1000 / 60);
+}
+tick();
