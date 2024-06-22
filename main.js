8b5b7407ce7a54bb60860ecc60d71a241d5e61a3
James Diffley <diffs1991@googlemail.com>
2024-06-22 17:56:19 +0100

    floor height generator created

--- main.js
+++ main.js
@@ -15,16 +15,26 @@ let world = {
     gravity: 10,
     speed: 2,
     distanceTravelled: 0,
+    highestFloor: 100,
     floorTiles: [
         new Floor(0, 140)
     ],
 
-    // move floor
+    // move floor loop to move continuously left (x axis)
     moveFloor: function() {
         for (let tile of this.floorTiles) {
             tile.x -= this.speed;
-            this.distanceTravelled += this.speed;
         }
+        this.distanceTravelled += this.speed;
+    },
+
+    // function to create varying heights of the tiles as moveFloor progresses
+    createFutureTiles: function() {
+        let previousTile = this.floorTiles[this.floorTiles.length - 1];
+        let randomHeight = Math.floor(Math.random() * this.highestFloor) + 20;
+        let leftValue = previousTile.x + previousTile.width;
+        let next = new Floor(leftValue, randomHeight);
+        this.floorTiles.push(next);
     },
 
     // function to continuously draw tiles as the game runs using a for loop
@@ -43,6 +53,9 @@ let world = {
 function tick() {
     world.draw();
     world.moveFloor();
+    if (world.floorTiles[world.floorTiles.length - 1].x + world.floorTiles[world.floorTiles.length - 1].width < world.width) {
+        world.createFutureTiles();
+    }
     window.setTimeout(tick, 1000 / 60);
 }
 tick();
