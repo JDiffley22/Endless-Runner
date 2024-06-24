20a8824d69440c21cffa7861a0a7da7d78f4dc87
James Diffley <diffs1991@googlemail.com>
2024-06-24 17:53:59 +0100

    Player Jumping added and all Jumps do not exceed player jump height

--- main.js
+++ main.js
@@ -15,6 +15,8 @@ let world = {
     gravity: 2,
     speed: 2,
     distanceTravelled: 0,
+    maxSpeed: 15,
+    tilesPassed: 0,
     highestFloor: 100,
     autoScroll: true,
     floorTiles: [
@@ -35,18 +37,29 @@ let world = {
 
     // function to create varying heights of the tiles as moveFloor progresses
     createFutureTiles: function() {
+        if (this.floorTiles.length >= 3) {
+            return;
+        }
         let previousTile = this.floorTiles[this.floorTiles.length - 1];
-        let randomHeight = Math.floor(Math.random() * this.highestFloor) + 20;
+        let biggestJumpableHeight = previousTile.height + player.height * 3.5;
+        if (biggestJumpableHeight > this.highestFloor) {
+            biggestJumpableHeight = this.highestFloor;
+        }
+        let randomHeight = Math.floor(Math.random() * biggestJumpableHeight) + player.height;
         let leftValue = previousTile.x + previousTile.width;
         let next = new Floor(leftValue, randomHeight);
         this.floorTiles.push(next);
     },
 
-    // function to remove tiles once they move past "x" by checking if they're less than or equal to the value of "x"  in the Floor function, this is to conserve the browser's memory
+    // function to remove tiles once they move past "x" by checking if they're less than or equal to the value of "x" in the Floor function (Conserve Browser Memory)
     removeTiles: function() {
         for (let index in this.floorTiles) {
             if (this.floorTiles[index].x <= -this.floorTiles[index].width) {
                 this.floorTiles.splice(index, 1);
+                this.tilesPassed++;
+                if (this.tilesPassed % 3 === 0 && this.speed < this.maxSpeed) {
+                    this.speed++;
+                }
             }
         }
     },
@@ -82,12 +95,15 @@ let world = {
     }
 };
 
-// player function
+// player functions
 let player = {
     x: 160,
     y: 340,
     height: 20,
     width: 20,
+    downwardForce: 0,
+    jumpHeight: 0,
+    isJumping: false,
 
     tick: function() {
         this.applyGravity();
@@ -98,26 +114,50 @@ let player = {
         ctx.fillRect(this.x, this.y - this.height, this.width, this.height);
     },
 
+    keyPress: function(event) {
+        if ((event.key === " " || event.key === "ArrowUp") && !this.isJumping) {
+            let floorHeight = world.getDistanceToFloor(this.x);
+            let onTheFloor = world.height - this.y === floorHeight;
+            if (onTheFloor) {
+                this.downwardForce = -25;
+                this.isJumping = true;
+            }
+        }
+    },
+
     // apply gravity to the player
     applyGravity: function() {
         let platformBelow = world.getDistanceToFloor(this.x);
         let rightHandSideDistance = world.getDistanceToFloor(this.x + this.width);
         this.currentDistanceAboveGround = world.height - this.y - platformBelow;
-
         if (this.currentDistanceAboveGround < 0 || rightHandSideDistance < 0) {
             world.stop();
         } else {
-            this.y += world.gravity;
+            this.y += this.downwardForce;
+            this.downwardForce += world.gravity;
+
             let floorHeight = world.getDistanceToFloor(this.x);
             let topOfPlatform = world.height - floorHeight;
             if (this.y > topOfPlatform) {
                 this.y = topOfPlatform;
+                this.downwardForce = 0;
+                this.isJumping = false;
+                this.jumpHeight = 0;
+            }
+
+            if (this.downwardForce < 0) {
+                this.jumpHeight += Math.abs(this.downwardForce);
+                if (this.jumpHeight >= player.height * 6) {
+                    this.downwardForce = world.gravity;
+                    this.jumpHeight = 0;
+                }
             }
         }
     }
 };
 
 // Call the draw function and set timer
+window.addEventListener("keydown", function(event) { player.keyPress(event); }, false);
 function tick() {
     player.tick();
     world.tick();
